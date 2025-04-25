1a7758ac1d4a8560755590ed5e1cb0c362a86c46
Manan <aggarwalmanan18@gmail.com>
2025-04-25 10:12:17 +0530

    25-4

--- src/components/expense.jsx
+++ src/components/expense.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const expense = () => {
     return (
-        <div className='flex flex-wrap gap-[3vw] items-center ml-[20vw] mt-[20vh] w-[75vw] justify-center     h-[160vh]'>
-            <div className="list card text-lg w-[21vw] h-[60vh] items-center ">
-                <div className='list text-xl font-bold justify-self-center px-10'>Date</div>
+        <div className='flex flex-wrap gap-[3vw] ml-[20vw] mt-[20vh] w-[75vw] justify-center h-[160vh]'>
+            <div className="list card relative text-lg w-[21vw] h-[60vh] items-center ">
+                <div className='list text-2xl font-bold justify-self-center px-10'>Date</div>
                 <div className='list bg-gray-700 h-1 w-full rounded-full mt-3 mb-5'></div>
                 <div className=" list flex justify-between my-2">
                     <div>items</div>
@@ -22,113 +22,12 @@ const expense = () => {
                     <div>items</div>
                     <div>price</div>
                 </div>
+                <div className='list bg-gray-700 h-1 w-[80%] justify-self-center rounded-full mt-3 mb-5'></div>
+                <div className='absolute font-extrabold text-2xl right-5'>Total</div>
             </div>
-            <div className="list card text-lg w-[21vw] h-[60vh] items-center ">
-                <div className='list text-xl font-bold justify-self-center '>Date</div>
-                <div className='list bg-gray-700 h-1 w-full rounded-full mt-3 mb-5'></div>
-                <div className=" list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-                <div className="  list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-                <div className="list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-                <div className="list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-            </div>
-
-            <div className="list card text-lg w-[21vw] h-[60vh] items-center ">
-                <div className='list text-xl font-bold justify-self-center '>Date</div>
-                <div className='list bg-gray-700 h-1 w-full rounded-full mt-3 mb-5'></div>
-                <div className=" list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-                <div className="  list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-                <div className="list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-                <div className="list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-            </div>
-
-            <div className="list card text-lg w-[21vw] h-[60vh] items-center ">
-                <div className='list text-xl font-bold justify-self-center '>Date</div>
-                <div className='list bg-gray-700 h-1 w-full rounded-full mt-3 mb-5'></div>
-                <div className=" list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-                <div className="  list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-                <div className="list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-                <div className="list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-            </div>
-
-            <div className="list card text-lg w-[21vw] h-[60vh] items-center ">
-                <div className='list text-xl font-bold justify-self-center '>Date</div>
-                <div className='list bg-gray-700 h-1 w-full rounded-full mt-3 mb-5'></div>
-                <div className=" list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-                <div className="  list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-                <div className="list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-                <div className="list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-            </div>
-
-            <div className="list card text-lg w-[21vw] h-[60vh] items-center ">
-                <div className='list text-xl font-bold justify-self-center '>Date</div>
-                <div className='list bg-gray-700 h-1 w-full rounded-full mt-3 mb-5'></div>
-                <div className=" list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-                <div className="  list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-                <div className="list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-                <div className="list flex justify-between my-2 ">
-                    <div>items</div>
-                    <div>price</div>
-                </div>
-            </div>
-
             
+
+
 
 
         </div>
--- src/components/total.jsx
+++ src/components/total.jsx
@@ -4,9 +4,9 @@ import './home.css'
 
 const home = () => {
   return (
-    <div className='ml-[20vw] mt-auto w-[65vw] h-100 justify-self-center rounded-3xl relative bg-amber-300'>
-      <div className='rounded-3xl bg-amber-700 h-100 w-[20vw] right-0 absolute '></div>
-     
+    <div className='total p-0 ml-[20vw] mt-auto w-[65vw] h-100 justify-self-center rounded-3xl relative bg-black/70'>
+      <div className='rounded-3xl bg-amber-700/80 h-100 w-[20vw] right-0 absolute '></div>
+      <div></div>
     </div>
   )
 }
