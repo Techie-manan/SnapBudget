21ec16e633e61f519702d7395bc1e36eb26ab00c
Manan <aggarwalmanan18@gmail.com>
2025-04-27 23:57:02 +0530

    27-4

--- src/components/addsnap.jsx
+++ src/components/addsnap.jsx
@@ -4,33 +4,52 @@ import { motion } from 'framer-motion';
 const addsnap = () => {
   return (
     <div className=" bg-home h-screen w-full p-0 m-0 overflow-hidden">
-      <div className='flex justify-center items-center'>
-        <motion.img
-        src='' alt=''
-         style={{        }}
-         animate={{ }}
-         transition={{}}
-         />
-
-        <motion.img
-        src='' alt=''
-         style={{        }}
-         animate={{ }}
-         transition={{}}
-         />
-
-        <motion.img
-        src='' alt=''
-         style={{        }}
-         animate={{ }}
-         transition={{}}
-         />
-
-      </div>
-
 
       {/* Content can go here if you have any */}
       <div className="relative h-full w-full">
+
+        <div className='flex gap-5 justify-center items-center h-full'>
+          <motion.img
+            src='src\assets\triangle.png' alt=''
+            animate={{
+              rotate: [0, 360],
+              y: [0, -50, 0],
+            }}
+            transition={{
+              duration: 2,
+              ease: "easeInOut"
+            }}
+          />
+
+          <motion.img
+            src='src\assets\circle.png' alt=''
+            animate={{
+              rotate: [0, 360],
+              y: [0, -50, 0],
+            }}
+            transition={{
+              delay:1,
+              duration: 2,
+              ease: "easeInOut"
+            }}
+          />
+
+          <motion.img
+            src='src\assets\square.png' alt=''
+            animate={{
+              rotate: [0, 360],
+              y: [0, -50, 0],
+            }}
+            transition={{
+              delay: 2,
+              duration: 2,
+            
+              ease: "easeInOut"
+            }}
+          />
+
+          <button className=''>Add Expense</button>
+        </div>
         {/* SVG at the bottom */}
         <div className="absolute -bottom-15 w-full bg-wave h-[40%] opacity-99 z-0">
 
